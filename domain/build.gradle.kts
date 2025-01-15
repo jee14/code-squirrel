bc3511a6701ff20c70a735bb6ca8200831c212ac
Seunghyeon Ji <3aroa2@gmail.com>
2025-01-15 22:18:37 +0900

    spring ai, openai 추가

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,4 +1,5 @@
 dependencies {
     compileOnly(libs.spring.context)
     implementation(libs.spring.tx)
+    implementation(platform(libs.spring.ai.bom))
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,12 @@ dependencyResolutionManagement {
     @Suppress("UnstableApiUsage")
     repositories {
         mavenCentral()
+        maven {
+            url = uri("https://repo.spring.io/milestone")
+        }
+        maven {
+            url = uri("https://repo.spring.io/snapshot")
+        }
         gradlePluginPortal()
     }
 }
