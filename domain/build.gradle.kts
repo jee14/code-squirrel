1ae0347fb596ac22122a3e2af2e91fae3ccbbc42
Seunghyeon Ji <3aroa2@gmail.com>
2025-01-15 22:29:41 +0900

    spring ai 의존성 implementation

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -2,4 +2,5 @@ dependencies {
     compileOnly(libs.spring.context)
     implementation(libs.spring.tx)
     implementation(platform(libs.spring.ai.bom))
+    implementation(libs.spring.ai.openai)
 }
